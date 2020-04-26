a339f867e01df2ec04ee30d37c6b450a68604221
Senka <senka.soic@uns.ac.rs>
2020-04-26 13:31:46 +0200

    Initial commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+   <div> 
+       <p>Home page</p>
+   </div>
+</template>
+
+<script>
+
+
+export default {
+    name: "HomePage",
+    components: {
+       
+    },
+    data() {
+      
+      
+    },
+    methods: {
+      
+    }
+  
+};
+</script>
+
+<style scoped>
+    
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HomePage from '../components/HomePage.vue'
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/homePage',
+            name: 'HomePage',
+            component: HomePage
+        },
+        {
+            path: '*',
+            redirect: '/'
+        }
+    ]
+})
