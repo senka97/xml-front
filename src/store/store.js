8e90f99089194ef5789d867cf8f3532b258bc312
MarinaBartulov <bartulovmarina1@gmail.com>
2020-05-16 16:41:02 +0200

    Https path for login changed.

--- src/store/store.js
+++ src/store/store.js
@@ -26,7 +26,7 @@ export const store =  new Vuex.Store({
     actions:{
         retrieveToken(context, credentials){
             return new Promise((resolve, reject) => {
-                axios.post('http://localhost:8081/auth/login',{
+                axios.post('https://localhost:8083/user/auth/login',{
                     email: credentials.email,
                     password: credentials.password
                 })
