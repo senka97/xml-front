836d2f20de5e150b73e369fcc325f546b1a90f7f
DjordjeRogic <rogic.djordje@gmail.com>
2020-07-11 20:06:20 +0200

    Added message for no cars

--- src/components/Statistic.vue
+++ src/components/Statistic.vue
@@ -4,7 +4,17 @@
     <br/>
     <h1 style="font-size:3em" align="Center">Statistics</h1>
     <br/>
-     <b-card-group style="margin:20px" deck>
+    <div  v-if="load()" align="center">
+      <br/>
+      <br/>
+     <b-spinner style="width: 5rem; height: 5rem;" label="Large Spinner"></b-spinner>
+    </div>
+    <br/>
+    <div v-if="!load()">
+    <div v-if="showEmpty()">
+      <h1 style="color:red">You don't have any cars yet!</h1>
+    </div>
+     <b-card-group v-if="!showEmpty()" style="margin:20px" deck>
     <b-card header-bg-variant="dark"  header-text-variant="white" :title="carTitle(kmDrivenCar.carBrand,kmDrivenCar.carModel)">
         <template bg-variant="warning" v-slot:header>
             <h2 class="mb-0">Most kilometers driven</h2>
@@ -192,6 +202,7 @@
     </b-card>
   </b-card-group>
     </div>
+    </div>
 </template>
 <script>
 
@@ -216,7 +227,6 @@ export default {
                 childrenSeats:0,
                 mileage:50000,
                 numberOfComments:10,
-                rate:5
              },
              mostCommentsCar:{
                 photos64:['https://images3.polovniautomobili.tv/user-images/thumbs/1358/13586411/1e33091352d0-800x600.jpg'],
@@ -229,7 +239,6 @@ export default {
                 childrenSeats:0,
                 mileage:50000,
                 numberOfComments:10,
-                rate:5
              },
              bestScoreCar:{
                 photos64:['https://images3.polovniautomobili.tv/user-images/thumbs/1358/13586411/1e33091352d0-800x600.jpg'],
@@ -242,28 +251,58 @@ export default {
                 childrenSeats:0,
                 mileage:50000,
                 numberOfComments:10,
-                rate:5
-             }
+
+             },
+             info: null,
+             returnedCar1:false,
+             returnedCar2:false,
+             returnedCar3:false
         }
     },
     methods:{
         carTitle(brand,model){
             return brand+" "+model
+        },
+          showEmpty(){
+            if(this.kmDrivenCar.rate == undefined || this.mostCommentsCar.rate == undefined || this.bestScoreCar.rate == undefined)
+            {
+                return true;
+            }
+        return false;
+        },
+        load(){
+            return !(this.returnedCar1 && this.returnedCar2 && this.returnedCar3);
         }
     },
     mounted(){
         axios.get(baseUrl+'/car/user/bestScore')
         .then(response => {
             this.bestScoreCar = response.data;
-        });
+            this.returnedCar1=true;
+        })
+        .catch(error =>{
+            this.info = error;
+            this.returnedCar1=true;
+        })
         axios.get(baseUrl+'/car/user/mostComments')
         .then(response => {
             this.mostCommentsCar = response.data;
-        });
+            this.returnedCar2=true;
+
+        })
+        .catch(error =>{
+            this.info = error;
+            this.returnedCar2=true;
+        })
         axios.get(baseUrl+'/car/user/mostKilometers')
         .then(response => {
             this.kmDrivenCar = response.data;
-        });
+            this.returnedCar3=true;
+        })
+        .catch(error =>{
+            this.info = error;
+            this.returnedCar3=true;
+        })
     }
 }
 </script>
